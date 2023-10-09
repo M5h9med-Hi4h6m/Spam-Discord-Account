4dd32a932f9bfa004103a42405013d153090a401
Mohamed Hisham <111419977+M5h9med-Hi4h6m@users.noreply.github.com>
2023-10-09 20:52:40 +0300

    Add files via upload

---
+++ Config.js
@@ -0,0 +1,7 @@
+// npm i @types/node discord.js-selfbot-v13 express node-fetch sphinx-run
+
+module.exports = {
+    Token: "", // You'r Account Token --- توكن حسابك
+    ChannelID: "", // Channel Id To Span --- الشانل الذي سوف يتم ارسال الاسبم فية
+    Type: "ar" // There is 2 types : eng | ar --- يوجد لغتين فقط لا اكثر عربي و الانجليزي
+}
---
+++ index.js
@@ -0,0 +1,44 @@
+const config = require("./Config");
+console.clear()
+async function bot(Token) {
+    console.log("By 6x.g");
+    const Discord = require("discord.js-selfbot-v13");
+    const client = new Discord.Client({
+        intents: [Discord.Intents.FLAGS.GUILDS],
+        checkUpdate: false,
+    });
+    client.on('ready', async () => {
+        console.log(`${client.user.username} Is Ready - By 6x.g`);
+        console.log("By 6x.g");
+    })
+    const { userAccount } = require("sphinx-run");
+    new userAccount(client, Discord).leveling({
+        channel: config.ChannelID,
+        randomLetters: false,
+        time: 1000,
+        type: config.Type
+    });
+    client.login(config.Token); 
+    console.log("By 6x.g");
+}
+
+bot(config.Token)
+
+// Express
+
+const express = require("express");
+const app = express();
+
+app.listen(() =>
+    console.log("Express Is Ready ")
+);
+
+app.use('/', (req, res) => { 
+    res.send(new Date());
+});
+
+// Return
+
+setInterval(() => {
+    return bot(config.Token);
+}, 30000)
