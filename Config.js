6489cc91bbec3e07012326a585afe1c852d5c484
Mohamed Hisham <111419977+M5h9med-Hi4h6m@users.noreply.github.com>
2023-10-09 20:53:22 +0300

    Update Config.js

--- Config.js
+++ Config.js
@@ -3,5 +3,5 @@
 module.exports = {
     Token: "", // You'r Account Token --- توكن حسابك
     ChannelID: "", // Channel Id To Span --- الشانل الذي سوف يتم ارسال الاسبم فية
-    Type: "ar" // There is 2 types : eng | ar --- يوجد لغتين فقط لا اكثر عربي و الانجليزي
+    Type: "eng" // There is 2 types : eng | ar --- يوجد لغتين فقط لا اكثر عربي و الانجليزي
 }
